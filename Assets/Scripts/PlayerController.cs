6b689b243bb94c85b04d9269fe583ad6e29c5cfa
everedx <carlos.serranoe1992@gmail.com>
2020-04-05 00:51:31 +1100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rBody;
+    [SerializeField] int impulseUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = new Vector2(0, 0);
+        if (Input.touchCount == 0)
+        {
+            
+        }
+        else
+        {
+            rBody.AddForce(new Vector3(0, impulseUp, 0));
+        }
+
+        
+    }
+}

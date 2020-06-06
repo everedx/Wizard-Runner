d85ca91e80ed77e4b4b3dc34e44b34dfa7afdbca
everedx <carlos.serranoe1992@gmail.com>
2020-06-06 23:53:45 +1000

    GOLD Mobile
    Second game done :)

--- Assets/Scripts/CyberJetPack/GUI/LanguageSelector.cs
+++ Assets/Scripts/CyberJetPack/GUI/LanguageSelector.cs
@@ -22,26 +22,38 @@ public class LanguageSelector : MonoBehaviour
 
     private void Start()
     {
+        float ratioWidth = (float)Screen.width / (float)Screen.currentResolution.width;
+        float ratioHeight = (float)Screen.height / (float)Screen.currentResolution.height;
+
         panelLanguages = new GameObject("PanelLanguages",new Type[] {typeof(RectTransform), typeof(CanvasRenderer), typeof(Image)});
         panelLanguages.transform.SetParent(gameObject.transform);
         panelLanguages.GetComponent<Image>().color = gameObject.GetComponent<Button>().colors.normalColor;
         RectTransform rect = panelLanguages.GetComponent<RectTransform>();
-        rect.sizeDelta = new Vector2( gameObject.GetComponent<RectTransform>().rect.width, gameObject.GetComponent<RectTransform>().rect.height*5);
-        rect.position = gameObject.transform.position + new Vector3(0, gameObject.GetComponent<RectTransform>().rect.height/2 + gameObject.GetComponent<RectTransform>().rect.height / 10 +rect.rect.height/2 , 0);
-        for( int i =0; i<pictures.Length;i++)
+        rect.sizeDelta = new Vector2( gameObject.GetComponent<RectTransform>().rect.width * ratioWidth, gameObject.GetComponent<RectTransform>().rect.height*5*ratioHeight);
+        //rect.position = gameObject.transform.position + new Vector3(0, gameObject.GetComponent<RectTransform>().rect.height/2 + gameObject.GetComponent<RectTransform>().rect.height / 10 + rect.rect.height / 2, 0);
+        rect.position = gameObject.transform.position + new Vector3(0, rect.sizeDelta.y/2 + ratioHeight*gameObject.GetComponent<RectTransform>().rect.height / 10 + ratioHeight* gameObject.GetComponent<RectTransform>().rect.height / 2, 0);
+        Image imHolder;
+        for ( int i =0; i<pictures.Length;i++)
         {
             GameObject button = Instantiate(buttonLanguagePf, panelLanguages.transform);
+            RectTransform rectButton = button.GetComponent<RectTransform>();
+            rectButton.sizeDelta = new Vector2(rectButton.sizeDelta.x * ratioWidth, rectButton.sizeDelta.y * ratioHeight);
+            imHolder = button.transform.GetChild(0).GetComponent<Image>();
+            imHolder.GetComponent<RectTransform>().anchoredPosition = new Vector2(imHolder.GetComponent<RectTransform>().anchoredPosition.x * ratioWidth, imHolder.GetComponent<RectTransform>().anchoredPosition.y);
+            imHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(imHolder.GetComponent<RectTransform>().sizeDelta.x * ratioWidth, imHolder.GetComponent<RectTransform>().sizeDelta.y * ratioHeight);
             button.name = pictures[i].name;
             button.transform.GetChild(0).GetComponent<Image>().sprite = pictures[i].image;
             button.transform.position = panelLanguages.transform.position + new Vector3(0, rect.rect.height/2 - button.GetComponent<RectTransform>().rect.height/2 - i* button.GetComponent<RectTransform>().rect.height, 0);
             button.GetComponent<Button>().onClick.AddListener(() => { childButtonClicked(button.transform.GetChild(0).GetComponent<Image>().sprite, button.name); });
         }
         panelLanguages.SetActive(false);
+        
         foreach (LanguageStruct st in pictures)
         {
             if (st.name.Equals(GameManager.instance.getLanguage()))
             {
-                gameObject.transform.GetChild(0).GetComponent<Image>().sprite = st.image;
+                imHolder = gameObject.transform.GetChild(0).GetComponent<Image>();
+                imHolder.sprite = st.image;
             }
         }
     }
--- Assets/Scripts/CyberJetPack/ProjectileWarning.cs
+++ Assets/Scripts/CyberJetPack/ProjectileWarning.cs
@@ -10,9 +10,12 @@ public class ProjectileWarning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float ratioScreenCurrent = (float)Screen.width / (float)Screen.height;
+        float ratioScreenCurrentResolution = (float)Screen.currentResolution.width / (float)Screen.currentResolution.height;
+        float ratioMultiplier = ratioScreenCurrentResolution / ratioScreenCurrent;
         if (projectileToWarn != null)
         {
-            transform.position = new Vector3(Camera.main.transform.position.x - 2 + Camera.main.orthographicSize * 2,projectileToWarn.transform.position.y,0);
+            transform.position = new Vector3(Camera.main.transform.position.x - 4 + Camera.main.orthographicSize * 2*ratioMultiplier,projectileToWarn.transform.position.y,0);
             if (projectileToWarn.position.x - Camera.main.transform.position.x < (Camera.main.orthographicSize * 2) + 2)
             {
                 Destroy(gameObject);
